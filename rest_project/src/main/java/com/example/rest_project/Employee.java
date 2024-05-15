24d82ccae366f1c03faa9e69966897202e1f8500
Enid Maharjan <46753597+strygwyr555@users.noreply.github.com>
2024-05-15 11:41:41 -0500

    cmmoit

--- rest_project/src/main/java/com/example/rest_project/Employee.java
+++ rest_project/src/main/java/com/example/rest_project/Employee.java
@@ -1,6 +1,10 @@
 package com.example.rest_project;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
@@ -19,6 +23,46 @@ public class Employee {
     @Column(name = "ssn")
     private String lastFourSsn;
     // Getters and setters
-    
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getLastFourSsn() {
+		return lastFourSsn;
+	}
+
+	public void setLastFourSsn(String lastFourSsn) {
+		this.lastFourSsn = lastFourSsn;
+	}
+ 
     
 }
